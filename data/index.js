c6117d953d49f11d93872c4fedf1e116d1bede26
Aymen Guedri <aymenguedri01@gmail.com>
2025-04-26 15:18:18 +0100

    fix

--- data/index.js
+++ data/index.js
@@ -27,5 +27,45 @@ export const users = [
     createdAt: 1115211422,
     updatedAt: 1115211422,
     __v: 0,
+  },
+  {
+    _id: userIds[1],
+    firstName: "sarra",
+    lastName: "sarra",
+    email: "test@gmail.com",
+    password: "$!FEAS@!O)_IDJda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "p3.jpeg",
+    friends: [],
+    location: "New York, CA",
+    occupation: "Degenerate",
+    viewedProfile: 12351,
+    impressions: 55555,
+    createdAt: 1595589072,
+    updatedAt: 1595589072,
+    __v: 0,
   }
+];
+
+export const posts = [
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[1],
+    firstName: "Aymen",
+    lastName: "guedri",
+    location: "Tn",
+    description: "Some really long random description",
+    picturePath: "post1.jpeg",
+    userPicturePath: "p3.jpeg",
+    likes: new Map([
+      [userIds[0], true],
+      [userIds[2], true],
+      [userIds[3], true],
+      [userIds[4], true],
+    ]),
+    comments: [
+      "random comment",
+      "another random comment",
+      "yet another random comment",
+    ],
+  },
 ];
